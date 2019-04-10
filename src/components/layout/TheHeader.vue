bf759991e9ee48b5d444aa5b93929a7004fa9f98
Federico Bozo <federico@priisma.com>
2019-04-10 17:28:42 -0400

    Header to component detailed design specs for Desktop

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,26 +1,69 @@
 <template>
+  <header class="header">
     <nav>
-      <div class="nav-wrapper">
-        <span href="#" class="brand-logo">Home</span>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li>
-            <a href="#">
-              <i class="material-icons dp48">notifications</i>
-            </a>
-          </li>
-          <li>
-            <a href="#">Sign out</a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="material-icons dp48">menu</i>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <a class="burger-menu" href="#">
+        <i class="material-icons dp48">menu</i>
+      </a>
+      <h3 class="title">Home</h3>
+      <ul id="nav-mobile" class="hide-on-med-and-down">
+        <li>
+          <a href="#">
+            <i class="material-icons dp48">notifications_none</i>
+          </a>
+        </li>
+        <li>
+          <a href="#">Sign out</a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="material-icons dp48">more_vert</i>
+          </a>
+        </li>
+      </ul>
+      <!-- <div class="nav-wrapper">
+      </div>-->
     </nav>
+  </header>
 </template>
 
 <script>
-  export default { name: "TheHeader" };
+export default { name: "TheHeader" };
 </script>
+
+<style lang="scss" scoped>
+nav {
+  display: flex;
+  align-items: center;
+  .burger-menu{
+    padding: 0 16px;
+  }
+  .title {
+    font-size: 1.4rem;
+    font-weight: 600;
+    letter-spacing: -0.065rem;
+    margin: 0 0 0 16px;
+  }
+  ul {
+    margin-left: auto;
+    li {
+      display: flex;
+      padding: 0;
+      &:not(:first-child):before {
+        content: "";
+        background: #2e3661;
+        width: 1px;
+        height: 23px;
+        display: flex;
+        align-self: center;
+      }
+    }
+    a {
+      font-size: 0.9rem;
+      display: flex;
+      text-transform: uppercase;
+      font-weight: 600;
+      letter-spacing: 0.035rem;
+    }
+  }
+}
+</style>
--- src/components/layout/TheLayout.vue
+++ src/components/layout/TheLayout.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="wrap">
-    <header class="header">
-      <slot name="header"></slot>
-    </header>
+    <slot name="header"></slot>
+
     <main class="main">
       <slot name="main"></slot>
     </main>
 
     <slot name="footer"></slot>
- 
   </div>
 </template>
 
 <script>
-    export default { name: "TheLayout" };
+  export default { name: "TheLayout" };
 </script>
