7fe378e14ea2fb4145529de6ed00c186997b40e1
Federico Bozo <federico@priisma.com>
2019-04-15 16:41:44 -0400

    Home stepper page activation with readable state variables. 3rd page open by data.

--- src/components/ui/BaseStepper.vue
+++ src/components/ui/BaseStepper.vue
@@ -1,49 +1,44 @@
 <template>
-<div class="flex xs12">
-  <v-stepper v-model="e6" vertical>
-    <v-stepper-step :complete="e6 > 1" step="1">
-      Welcome
-      <small>You're really close to your dreams</small>
-    </v-stepper-step>
+  <div class="flex xs12">
+    <v-stepper v-model="pageActivation" vertical>
+      <v-stepper-step :complete="true" step="1" @click="pageActivation = 1">
+        Welcome
+        <small>You're really close to your dreams</small>
+      </v-stepper-step>
 
-    <v-stepper-content step="1">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click="e6 = 2">Continue</v-btn>
-      <v-btn flat>Cancel</v-btn>
-    </v-stepper-content>
+      <v-stepper-content step="1">
+        <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+      </v-stepper-content>
 
-    <v-stepper-step :complete="e6 > 2" step="2">Pre-closing</v-stepper-step>
+      <v-stepper-step :complete="true" step="2" @click="pageActivation = 2">Pre-closing</v-stepper-step>
 
-    <v-stepper-content step="2">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
-      <v-btn flat>Cancel</v-btn>
-    </v-stepper-content>
+      <v-stepper-content step="2">
+        <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+      </v-stepper-content>
 
-    <v-stepper-step :complete="e6 > 3" step="3">Permission to operate</v-stepper-step>
+      <v-stepper-step :complete="false" step="3" @click="pageActivation = 3">Permission to operate</v-stepper-step>
 
-    <v-stepper-content step="3">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click="e6 = 4">Continue</v-btn>
-      <v-btn flat>Cancel</v-btn>
-    </v-stepper-content>
+      <v-stepper-content step="3">
+        <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+      </v-stepper-content>
 
-    <v-stepper-step step="4">Activation</v-stepper-step>
-    <v-stepper-content step="4">
-      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-      <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
-      <v-btn flat>Cancel</v-btn>
-    </v-stepper-content>
-  </v-stepper>
-</div>
+      <v-stepper-step :complete="false" step="4" @click="pageActivation = 4">Activation</v-stepper-step>
+
+      <v-stepper-content step="4">
+        <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+      </v-stepper-content>
+    </v-stepper>
+  </div>
 </template>
 
 <script>
-export default { name: "BaseStepper",
-  data () {
-      return {
-        e6: 1
-      }
-    } 
-  };
+export default {
+  name: "BaseStepper",
+  data() {
+    return {
+      // opens third page
+      pageActivation: 3
+    };
+  }
+};
 </script>
