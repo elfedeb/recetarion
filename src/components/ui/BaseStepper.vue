e43ac96385e35110e546e5e6350e4c57b5c49db4
Federico Bozo <federico@priisma.com>
2019-04-16 11:38:11 -0400

    - Added Alert Box - Update stepper styles to the stylesheet

--- src/components/ui/BaseStepper.vue
+++ src/components/ui/BaseStepper.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="flex xs12 fluid">
-    <v-stepper v-model="pageActivation" vertical class="journey-stepper">
+    <v-alert
+      v-model="alert"
+      dismissible
+      type="success"
+      color="lennar-alert"
+      icon="warning"
+    >This is a success alert that is closable.</v-alert>
 
+    <v-stepper v-model="pageActivation" vertical class="journey-stepper">
       <!---------------- WELCOME ----------------->
       <v-stepper-step :complete="true" step="1" @click="pageActivation = 1">
-        <v-layout xs12 row class='full-width' wrap>
-          <v-flex xs12 md3><h3 class='v-stepper-label__header'>Welcome</h3></v-flex>
-          <v-flex xs12 md9><span class="body-2 font-weight-thin">Learn about your solar system and the Sunstreet program</span></v-flex>
+        <v-layout xs12 row class="full-width" wrap>
+          <v-flex xs12 md3>
+            <h3 class="v-stepper-label__header">Welcome</h3>
+          </v-flex>
+          <v-flex xs12 md9>
+            <span
+              class="body-2 font-weight-thin"
+            >Learn about your solar system and the Sunstreet program</span>
+          </v-flex>
         </v-layout>
       </v-stepper-step>
 
       <v-stepper-content step="1">
-        <v-card color="grey lighten-1" class="mb-5" height="200px">
-          
-        </v-card>
+        <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
       </v-stepper-content>
       <!------------------------------------------->
 
       <!---------------- PRE-CLOSING -------------->
       <v-stepper-step :complete="true" step="2" @click="pageActivation = 2">
-        <v-layout xs12 row class='full-width' wrap>
-          <v-flex xs12 md3><h3 class='v-stepper-label__header'>Pre-closing</h3></v-flex>
-          <v-flex xs12 md9><span class="body-2 font-weight-thin">Learn about pre-closing</span></v-flex>
+        <v-layout xs12 row class="full-width" wrap>
+          <v-flex xs12 md3>
+            <h3 class="v-stepper-label__header">Pre-closing</h3>
+          </v-flex>
+          <v-flex xs12 md9>
+            <span class="body-2 font-weight-thin">Learn about pre-closing</span>
+          </v-flex>
         </v-layout>
       </v-stepper-step>
 
@@ -30,10 +45,13 @@
         <v-layout wrap>
           <v-flex xs12 sm6 md4 lg3>
             <v-hover>
-              <v-card flat @click.stop="dialog = true"
+              <v-card
+                flat
+                @click.stop="dialog = true"
                 slot-scope="{ hover }"
                 :class="`elev elevation-${hover ? 4 : 0}`"
-                class="mx-auto">
+                class="mx-auto"
+              >
                 <v-card-title>
                   <h4>How solar works</h4>
                 </v-card-title>
@@ -49,10 +67,13 @@
           </v-flex>
           <v-flex xs12 sm6 md4 lg3>
             <v-hover>
-              <v-card flat @click.stop="dialog = true"
+              <v-card
+                flat
+                @click.stop="dialog = true"
                 slot-scope="{ hover }"
                 :class="`elev elevation-${hover ? 4 : 0}`"
-                class="mx-auto">
+                class="mx-auto"
+              >
                 <v-card-title>
                   <h4>How solar works</h4>
                 </v-card-title>
@@ -68,10 +89,13 @@
           </v-flex>
           <v-flex xs12 sm6 md4 lg3>
             <v-hover>
-              <v-card flat @click.stop="dialog = true"
+              <v-card
+                flat
+                @click.stop="dialog = true"
                 slot-scope="{ hover }"
                 :class="`elev elevation-${hover ? 4 : 0}`"
-                class="mx-auto">
+                class="mx-auto"
+              >
                 <v-card-title>
                   <h4>How solar works</h4>
                 </v-card-title>
@@ -102,11 +126,15 @@
       </v-stepper-content>
       <!------------------------------------------->
 
-      <!---------- PERMISSION TO OPERATE ---------->      
+      <!---------- PERMISSION TO OPERATE ---------->
       <v-stepper-step :complete="false" step="3" @click="pageActivation = 3">
-        <v-layout xs12 row class='full-width' wrap>
-          <v-flex xs12 md3><h3 class='v-stepper-label__header'>Permission to operate</h3></v-flex>
-          <v-flex xs12 md9><span class="body-2 font-weight-thin">Must complete the previous phases to unlock this</span></v-flex>
+        <v-layout xs12 row class="full-width" wrap>
+          <v-flex xs12 md3>
+            <h3 class="v-stepper-label__header">Permission to operate</h3>
+          </v-flex>
+          <v-flex xs12 md9>
+            <span class="body-2 font-weight-thin">Must complete the previous phases to unlock this</span>
+          </v-flex>
         </v-layout>
       </v-stepper-step>
 
@@ -115,11 +143,15 @@
       </v-stepper-content>
       <!-------------------------------------------->
 
-      <!---------------- ACTIVATION ------0--------->      
+      <!---------------- ACTIVATION ------0--------->
       <v-stepper-step :complete="false" step="4" @click="pageActivation = 4">
-        <v-layout xs12 row class='full-width' wrap>
-          <v-flex xs12 md3><h3 class='v-stepper-label__header'>Activation</h3></v-flex>
-          <v-flex xs12 md9><span class="body-2 font-weight-thin">Must complete the previous phases to unlock this</span></v-flex>
+        <v-layout xs12 row class="full-width" wrap>
+          <v-flex xs12 md3>
+            <h3 class="v-stepper-label__header">Activation</h3>
+          </v-flex>
+          <v-flex xs12 md9>
+            <span class="body-2 font-weight-thin">Must complete the previous phases to unlock this</span>
+          </v-flex>
         </v-layout>
       </v-stepper-step>
 
@@ -127,7 +159,6 @@
         <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
       </v-stepper-content>
       <!------------------------------------------->
-
     </v-stepper>
 
     <v-dialog v-model="dialog" max-width="592">
@@ -154,36 +185,9 @@ export default {
     return {
       // opens third page
       pageActivation: 2,
-      dialog: false
+      dialog: false,
+      alert: true
     };
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.v-stepper-label__header {
-  font-size: 15px;
-}
-
-.v-stepper__content {
-  h3 {
-    color: #696969;
-    margin: 2rem 0;
-  }
-}
-.v-stepper__content .v-card__title {
-  padding: 12px;
-  color: #393939;
-}
-
-.elev {
-  cursor: pointer;
-}
-
-</style>
-
-<style lang="scss">
-  .journey-stepper .v-stepper__label {
-    width: 100%;
-  }
-</style>
