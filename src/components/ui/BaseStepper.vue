857e00c24684b18472f995a8ebe135464164eb85
Federico Bozo <federico@priisma.com>
2019-04-16 11:59:34 -0400

    Stepper: - FAQ expansion to match the card border - Title font and color

--- src/components/ui/BaseStepper.vue
+++ src/components/ui/BaseStepper.vue
@@ -111,11 +111,11 @@
           </v-flex>
         </v-layout>
         <h3>FAQ</h3>
-        <v-card>
+        <v-card flat>
           <v-expansion-panel>
             <v-expansion-panel-content v-for="(item,i) in 5" :key="i">
               <template v-slot:header>
-                <div>FAQ title</div>
+                <h4>FAQ title</h4>
               </template>
               <v-card>
                 <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
