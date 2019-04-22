85ee95241ac6c20da6d1acf03b773e4e66d266b6
Federico Bozo <federico@priisma.com>
2019-04-22 10:05:15 -0400

    FAQ data from json file

--- src/views/Dashboard/PageHelpFAQ.vue
+++ src/views/Dashboard/PageHelpFAQ.vue
@@ -43,29 +43,16 @@
 
 <script>
 import FAQExpansion from "@/components/ui/FAQExpansion.vue";
-import ItemCard from "@/components/ui/ItemCard.vue";
-// import dataFAQ from "@/data/data-faqs.js";
+import dataFAQ from "@/data/data-faq.js";
 
 export default {
   name: "PageHelpFAQ",
   components: {
-    FAQExpansion,
-    "item-card": ItemCard
+    FAQExpansion
   },
   data() {
     return {
-      // data: dataFAQ,
-      faqItems: [
-        {
-          question: "What is one plus one?",
-          answer:
-            '<p>Donec commodo erat sed nibh blandit, sed lobortis orci vestibulum. Duis pulvinar eu orci nec ornare. Cras convallis ornare dolor, eu <a href="#!">euismod nulla mollis sit amet</a>. In congue elit ac felis dapibus tempus. Sed vel leo eget metus luctus feugiat. Aenean id lacus nec nibh imperdiet laoreet. Sed accumsan a metus ac pellentesque.</p> <p>Vestibulum eget tortor in magna tristique interdum vitae at orci. Suspendisse ac nibh in mi tincidunt eleifend quis quis odio. Nullam at elit malesuada, aliquam mi a, vulputate urna. Nullam auctor lectus interdum pharetra egestas. Maecenas vestibulum lacinia posuere. Aliquam auctor nunc lorem, et interdum ipsum pulvinar vitae. Nulla elit dolor, laoreet eget tellus nec, fermentum tristique dui.</p>'
-        },
-        {
-          question: "Where can I get a real question?",
-          answer: "By intersecting different curiosities and realities"
-        }
-      ],
+      faqItems: dataFAQ,
       selectedCategory: this.$route.query.tag,
       breadcrumbs: [
         {
