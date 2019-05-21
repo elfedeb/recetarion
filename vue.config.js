57e1b1c9dba637f05051aee71b002d1eb87addeb
Federico Bozo <federico.bozo@gmail.com>
2019-05-21 09:50:38 -0400

    first auto deployment try

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  css: {
-    sourceMap: true
-  },
-  devServer: {
-    host: 'localhost',
-    https: true
-  }
+    publicPath: process.env.NODE_ENV === 'production' ? '/recetarion/' : '/',
+    css: {
+        sourceMap: true
+    },
+    devServer: {
+        host: 'localhost',
+        https: true
+    }
 }
